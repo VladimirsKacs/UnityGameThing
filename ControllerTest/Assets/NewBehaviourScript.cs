0de5c42e44b1fd0308dc5f6919279406f39d34f8
VladimirsKacs <VladimirsKacs@gmail.com>
2015-05-21 23:56:57 +0300

    initial

---
+++ ControllerTest/Assets/NewBehaviourScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float horizontalSpeed;
+	public bool canJump;
+
+	// Use this for initialization
+	void Start () {
+		horizontalSpeed = 0;
+		Debug.Log ("start");
+		foreach (var collider in this.GetComponents<BoxCollider2D>())
+			Debug.Log ("collider: "+collider.name);
+	}
+
+	private bool onTheFloor()
+	{
+		Debug.Log ("is it on the floor?");
+		Debug.Log(transform.position);
+		var renderer = this.GetComponent<Renderer> ();
+		float semiHeight = renderer.bounds.extents.x;
+		RaycastHit2D hit = Physics2D.Raycast (transform.position-Vector3.up*(semiHeight+0.001f), -Vector3.up, 10);
+		Debug.Log(hit.collider.name+"@"+hit.point+" ("+ hit.distance+" )");
+		if (hit.distance<0.1) {
+			Debug.Log ("yup.");
+			return true;
+		}
+		return false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		horizontalSpeed *= 0.75f;
+		if (Input.GetKey ("left"))
+			horizontalSpeed -= 0.05f; 
+		if (Input.GetKey ("right"))
+			horizontalSpeed += 0.05f;
+		if (Input.GetKeyDown ("up")&&onTheFloor()) 
+			this.GetComponent<Rigidbody2D>().AddForce (Vector2.up * 300);
+		this.transform.Translate (Vector3.right * horizontalSpeed);
+
+		//OnCollisionEnter (null);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("collision!");
+		canJump = true;		
+	}
+
+	void OnCollisionEnxit(Collision collision) {
+		Debug.Log ("No collision!");
+		canJump = false;		
+	}
+
+
+
+}
---
+++ ControllerTest/Assets/StalkerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StalkerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (Input.GetKey ("space")) {
+			Debug.Log("down");
+			var player = GameObject.FindWithTag ("Player");
+			var centerOfPlayer = player.transform.position;//+Vector3.up*327+Vector3.right*168;
+			transform.position = centerOfPlayer;
+			GetComponentInChildren<Image>().enabled=true;
+		} else {
+			Debug.Log("up");
+			var img=GetComponentInChildren<Image>().enabled=false;
+		}
+		*/
+		bool down = Input.GetKeyDown("space");
+		bool held = Input.GetKey("space");
+		bool up = Input.GetKeyUp("space");
+	
+		Debug.Log("Now: " + down + ", " + held + ", " + up);
+	}
+}
