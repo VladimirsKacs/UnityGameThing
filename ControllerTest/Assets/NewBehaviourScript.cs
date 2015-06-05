154d2fb48beb4c8ed3f1dcc1b8b938184ab3db61
Lat1sh <aleksandrsterentjevs@gmail.com>
2015-06-05 21:51:09 +0300

    added interactable class
    added interactable class

--- ControllerTest/Assets/NewBehaviourScript.cs
+++ ControllerTest/Assets/NewBehaviourScript.cs
@@ -11,33 +11,27 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//horizontalSpeed = 0;
+
 		horizontalSpeed = 3.5f;
-		Debug.Log (Time.deltaTime);
-		Debug.Log (Time.fixedDeltaTime);
-		//Time.timeScale = 0.1f;
-		Debug.Log (Time.deltaTime);
-		Debug.Log (Time.fixedDeltaTime);
-		//Time.fixedDeltaTime *= 0.5f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (Time.deltaTime);
 
-		if (Input.GetKey ("left") || (Input.GetAxis ("Horizontal") < 0)) {
-			Debug.Log("LEFT");
-			this.transform.position -= new Vector3 (horizontalSpeed * Time.deltaTime, 0.0f, 0.0f);
+		if (Input.GetKey ("left")) {
+			this.transform.position -= new Vector3(horizontalSpeed * Time.deltaTime, 0.0f, 0.0f);
 		}
 
-		if (Input.GetKey ("right")||(Input.GetAxis("Horizontal")>0))
+		if (Input.GetKey ("right"))
 			this.transform.position += new Vector3(horizontalSpeed * Time.deltaTime, 0.0f, 0.0f);
 
 		if (Input.GetKeyDown ("up") && allowedToJump()) 
 			this.GetComponent<Rigidbody2D>().AddForce (Vector2.up * 300);
 
-		if (Input.GetButtonDown ("OpenMenu"))
-			Debug.Log ("UseSOuls");
+		if (Input.GetKey("r")) {
+			Application.LoadLevel("ControllerTest");
+		}
 	}
 
 	bool allowedToJump() {
